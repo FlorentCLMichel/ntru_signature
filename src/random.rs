1aa481bc58d944eb4333787e8aa797a929136834
Florent C. L. M. Michel <56166507+FlorentCLMichel@users.noreply.github.com>
2022-06-03 11:03:56 +0100

    correct a typo

--- src/random.rs
+++ src/random.rs
@@ -5,7 +5,7 @@ const MAX_ARRAY_SIZE: usize = 32;
 
 /// fill an array of `u8`s with random values
 ///
-/// We use `thread_rng` rom the `rand` crate. 
+/// We use `thread_rng` from the `rand` crate. 
 /// Internally, it uses a ChaCha block cipher with 12 rounds as PRNG, seeded by the OS RNG.
 pub fn fill_rand_bytes(a: &mut [u8]) {
     a.chunks_mut(MAX_ARRAY_SIZE)
